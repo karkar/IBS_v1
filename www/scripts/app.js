45fc52fc4e3fee59b9295015f629c25d045dea92
karkar <ravikarkar@gmail.com>
2015-02-25 17:18:17 -0800

    Added modal to the header_test page

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -85,3 +85,31 @@ app.config(function($stateProvider, $urlRouterProvider) {
 });
 
 
+app.controller('MainCtrl', function($scope, $ionicModal) {
+  $scope.contact = {
+    name: 'Mittens Cat',
+    info: 'Tap anywhere on the card to open the modal'
+  }
+
+  $ionicModal.fromTemplateUrl('templates/modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal
+  })  
+
+  $scope.openModal = function() {
+    $scope.modal.show()
+  }
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+})
+
+
+
